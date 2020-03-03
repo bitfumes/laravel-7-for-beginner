961ed0ddec9d14096b13c26a2aacaa556f3fab5a
sarthaksavvy <bitfumes@gmail.com>
2020-03-04 02:21:53 +0530

    database connection and migration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,9 +2,8 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
