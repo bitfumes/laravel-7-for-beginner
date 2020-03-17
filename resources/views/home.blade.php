db10188d65055b5835f449121574a5f640c22c0d
sarthaksavvy <bitfumes@gmail.com>
2020-03-17 12:26:40 +0530

    configurations done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('body')
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,7 @@
         </nav>
 
         <main class="py-4">
-            @yield('body')
+            @yield('content')
         </main>
     </div>
 </body>
