670312bef23c41f03d189f4bce2a66961f68980a
sarthaksavvy <bitfumes@gmail.com>
2020-03-20 11:19:38 +0530

    laravel livewire 1

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -17,6 +17,15 @@
         <div class="py-1">
         <textarea name="description" class="p-2 rounded border" placeholder="Description"></textarea>
         </div>
+        
+        <div class="py-2">
+
+        <div class="flex justify-center pb-4 px-4">
+            <h2 class="text-lg pb-4">Add Steps for task</h2>
+            <span class="fas fa-plus px-2 py-1 cursor-pointer" />
+        </div>
+        <input type="text" name="step" class="py-2 px-2 border rounded" placeholder="Describe Step"/>
+        </div>
         <div class="py-1">
         <input type="submit" value="Create" class="p-2 border rounded" />
         </div>
--- resources/views/todos/layout.blade.php
+++ resources/views/todos/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+    @livewireStyles
     <title>Todos</title>
 </head>
 <body>
@@ -13,5 +14,7 @@
         @yield('content')
         </div>
     </div>
+
+    @livewireScripts
 </body>
 </html>
